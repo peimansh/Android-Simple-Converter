0bb81ababaf05a8b5b00683d6fed1b777e69ac6a
torinorgist <32059257+torinorgist@users.noreply.github.com>
2017-11-03 20:21:25 +0100

    Day 2 - Exercise 2 - PemiConverter V2

--- app/src/main/java/com/example/tmp_sda_1130/pemiconverter/MainActivity.java
+++ app/src/main/java/com/example/tmp_sda_1130/pemiconverter/MainActivity.java
@@ -2,57 +2,76 @@ package com.example.tmp_sda_1130.pemiconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.logging.Logger;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayAdapter adapter;
+    private ConvertController controller;
+    private Spinner spinner ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //View myView = getLayoutInflater().inflate(android.R.layout.simple_spinner_item,  null);
+
+        controller = new ConvertController();
+        //Log.i("hello",Integer.toString(R.id.convsel));
+        spinner = (Spinner)findViewById(R.id.convsel);
+        //spinner = (Spinner)myView.findViewById(R.id.convsel);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, controller.getAll());
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
     }
+
 
     public void Convert(View view) {
 
         EditText editText1 = (EditText)findViewById(R.id.meter);
-        String temp = editText1.getText().toString();
-        String tmp = null;
-        double temp1 = 0;
+        EditText editText2 = (EditText)findViewById(R.id.feet);
+        String temp1 = editText1.getText().toString();
+        int v = spinner.getSelectedItemPosition();
+        String value = (String) spinner.getItemAtPosition(v);
+        double tmp = Double.parseDouble(temp1);
+        double feet = tmp*(3.28084);
+        double inch = feet*12;
+        double yard = feet/3;
 
         try {
-            if (view.getId() == R.id.conversion) {
-                temp1 = Double.parseDouble(editText1.getText().toString());
+            if (value.equals("Convert from Meter to Feet")) {
+                editText2.setText(feet+" Feet");
+            }
+            else if (value.equals("Convert from Meter to Inch")) {
+                editText2.setText(inch+" Inch");
+            }
+            else if (value.equals("Convert from Meter to Yard")) {
+                editText2.setText(yard+" Yard");
             }
         }
         catch (NumberFormatException ex) {
             Toast t1 = Toast.makeText(this, "You should enter a number" ,Toast.LENGTH_LONG);
             t1.setGravity(Gravity.TOP| Gravity.RIGHT, 25, 200);
             t1.show();
-            EditText editText2 = (EditText)findViewById(R.id.feet);
             editText2.setText("0");
-            EditText editText3 = (EditText)findViewById(R.id.inch);
-            editText3.setText("0");
-            EditText editText4 = (EditText)findViewById(R.id.yard);
-            editText4.setText("0");
             return;
         }
 
-        double feet = temp1*(3.28084);
-        double inch = feet*12;
-        double yard = feet/3;
-        EditText editText2 = (EditText)findViewById(R.id.feet);
-        editText2.setText(feet+" Feet.");
-        EditText editText3 = (EditText)findViewById(R.id.inch);
-        editText3.setText(inch+" Inches.");
-        EditText editText4 = (EditText)findViewById(R.id.yard);
-        editText4.setText(yard+" Yards.");
-        Toast toastfeet = Toast.makeText(this,"conversion done !",Toast.LENGTH_SHORT);
-        toastfeet.setGravity(Gravity.TOP| Gravity.RIGHT, 25, 450);
-        toastfeet.show();
-
     }
+
+
+
+
+
 
 }
